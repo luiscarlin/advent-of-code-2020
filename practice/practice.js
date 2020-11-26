5a9ecb95d3e21fd1a18845117daf090ea9223516
Luis Carlin <luiscarlin@gmail.com>
2020-11-25 23:02:16 -0500

    small edits

--- practice/practice.js
+++ practice/practice.js
@@ -1,12 +1,10 @@
-#!/usr/bin/env node
-
 import fs from 'fs';
 import _ from 'lodash';
 
 const lines = fs
-  .readFileSync(`./practice.in`, 'utf8')
+  .readFileSync(`./practice/practice.in`, 'utf8')
   .split('\n')
   .map(Number)
   .map((num) => Math.floor(num / 3) - 2);
 
-console.log(_.sum(lines));
+console.log('part 1', _.sum(lines));
