28122d7df784b2582ecb3039ad3f353610acbd58
Luis Carlin <luiscarlin@gmail.com>
2020-12-01 00:23:56 -0500

    part 1 and 2 of day 1 done

---
+++ day-01/1.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+
+const part1 = () => {
+  const nums = fs.readFileSync('./day-01/1.in', 'utf8').split('\n').map(Number);
+
+  let num1,
+    num2 = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    nums[i];
+
+    for (let j = 0; j < nums.length; j += 1) {
+      if (nums[i] !== nums[j]) {
+        if (nums[i] + nums[j] === 2020) {
+          num1 = nums[i];
+          num2 = nums[j];
+          break;
+        }
+      }
+    }
+  }
+
+  return num1 * num2;
+};
+
+const part2 = () => {
+  const nums = fs.readFileSync('./day-01/1.in', 'utf8').split('\n').map(Number);
+
+  let num1,
+    num2,
+    num3 = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = 0; j < nums.length; j += 1) {
+      for (let z = 0; z < nums.length; z += 1) {
+        if (nums[i] + nums[j] + nums[z] === 2020) {
+          num1 = nums[i];
+          num2 = nums[j];
+          num3 = nums[z];
+          break;
+        }
+      }
+    }
+  }
+
+  return num1 * num2 * num3;
+};
+
+console.log('part 1', part1());
+console.log('part 2', part2());
