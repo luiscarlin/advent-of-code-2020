ccfc5ec9ef58003d48686171f28b4b3c8bbe9382
Luis Carlin <luiscarlin@gmail.com>
2020-12-01 00:58:29 -0500

    more refactoring

--- day-01/1.js
+++ day-01/1.js
@@ -3,34 +3,21 @@ import fs from 'fs';
 const part1 = () => {
   const nums = fs.readFileSync('./day-01/1.in', 'utf8').split('\n').map(Number);
 
-  let num1,
-    num2 = 0;
-
   for (let i = 0; i < nums.length; i += 1) {
-    nums[i];
-
     for (let j = 0; j < nums.length; j += 1) {
       if (i === j) {
         continue;
       }
 
       if (nums[i] + nums[j] === 2020) {
-        num1 = nums[i];
-        num2 = nums[j];
-        break;
+        return nums[i] * nums[j];
       }
     }
   }
-
-  return num1 * num2;
 };
 
 const part2 = () => {
   const nums = fs.readFileSync('./day-01/1.in', 'utf8').split('\n').map(Number);
-
-  let num1,
-    num2,
-    num3 = 0;
 
   for (let i = 0; i < nums.length; i += 1) {
     for (let j = 0; j < nums.length; j += 1) {
@@ -40,16 +27,11 @@ const part2 = () => {
         }
 
         if (nums[i] + nums[j] + nums[z] === 2020) {
-          num1 = nums[i];
-          num2 = nums[j];
-          num3 = nums[z];
-          break;
+          return nums[i] * nums[j] * nums[z];
         }
       }
     }
   }
-
-  return num1 * num2 * num3;
 };
 
 console.log('part 1', part1());
