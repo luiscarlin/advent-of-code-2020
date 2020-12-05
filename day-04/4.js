f820c281b93c9fd77d99006e982e911ae56cb2ce
Luis Carlin <luiscarlin@gmail.com>
2020-12-04 23:47:03 -0500

    refactor using what msomji did

--- day-04/4.js
+++ day-04/4.js
@@ -1,173 +1,86 @@
 import fs from 'fs';
 
-const checker = (arr, target) => target.every((v) => arr.includes(v));
+const REQUIRED_PASSPORT_FIELDS = [
+  'byr',
+  'iyr',
+  'eyr',
+  'hgt',
+  'hcl',
+  'ecl',
+  'pid',
+];
 
-const part1 = () => {
-  const lines = fs.readFileSync('./day-04/4.in', 'utf8').split('\n');
+const EYE_COLOR = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
 
-  const passports = [];
-  let index = 0;
+const valueIsBetween = (num, min, max) => num >= min && num <= max;
 
-  lines.forEach((line) => {
-    if (line === '') {
-      index += 1;
-    } else {
-      if (passports[index]) {
-        passports[index] += ' ' + line;
-      } else {
-        passports[index] = line;
-      }
-    }
-  });
+const isValidHgt = (hgt) => {
+  const number = Number(hgt.slice(0, -2));
+  const unit = hgt.slice(-2);
 
-  let numValid = 0;
-
-  passports.forEach((passport) => {
-    if (
-      passport.includes('byr') &&
-      passport.includes('iyr') &&
-      passport.includes('eyr') &&
-      passport.includes('hgt') &&
-      passport.includes('hcl') &&
-      passport.includes('ecl') &&
-      passport.includes('pid') &&
-      passport.includes('cid')
-    ) {
-      numValid += 1;
-    } else if (
-      passport.includes('byr') &&
-      passport.includes('iyr') &&
-      passport.includes('eyr') &&
-      passport.includes('hgt') &&
-      passport.includes('hcl') &&
-      passport.includes('ecl') &&
-      passport.includes('pid')
-    ) {
-      numValid += 1;
-    }
-  });
-  return numValid;
-};
-
-const part2 = () => {
-  const lines = fs.readFileSync('./day-04/4.in', 'utf8').split('\n');
-
-  const passports = [];
-  let index = 0;
-
-  lines.forEach((line) => {
-    if (line === '') {
-      index += 1;
-    } else {
-      if (passports[index]) {
-        passports[index] += ' ' + line;
-      } else {
-        passports[index] = line;
-      }
-    }
-  });
-
-  const dict = passports.map((passport) => {
-    const obj = {};
-    passport.split(' ').forEach((entry) => {
-      const [key, val] = entry.split(':');
-      obj[key] = val;
-    });
-
-    return obj;
-  });
-
-  const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-
-  let numValid = 0;
-
-  for (let i = 0; i < dict.length; i += 1) {
-    const current = dict[i];
-
-    // required.every((c) => Object.keys(current).includes(c));
-
-    if (!required.every((c) => Object.keys(current).includes(c))) {
-      continue;
-    }
-
-    //four digits; at least 1920 and at most 2002.
-    if (current.byr.length !== 4) {
-      continue;
-    }
-    const byrNumber = Number(current.byr);
-    if (byrNumber < 1920 || byrNumber > 2002) {
-      continue;
-    }
-
-    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    if (current.iyr.length !== 4) {
-      continue;
-    }
-
-    const iyrNumber = Number(current.iyr);
-    if (!(iyrNumber >= 2010 && iyrNumber <= 2020)) {
-      continue;
-    }
-
-    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030
-    if (current.eyr.length !== 4) {
-      continue;
-    }
-    const eyrNumber = Number(current.eyr);
-    if (!(eyrNumber >= 2020 && eyrNumber <= 2030)) {
-      continue;
-    }
-
-    // hgt (Height) - a number followed by either cm or in:
-    // If cm, the number must be at least 150 and at most 193.
-    // If in, the number must be at least 59 and at most 76.
-
-    // console.log(Object.keys(current));
-    // console.log(current.hgt);
-    const number = Number(current.hgt.slice(0, -2));
-    const unit = current.hgt.slice(-2);
-
-    if (!['cm', 'in'].includes(unit)) {
-      continue;
-    }
-
-    if (unit === 'cm') {
-      if (number < 150 || number > 193) {
-        continue;
-      }
-    } else {
-      if (number < 59 || number > 76) {
-        continue;
-      }
-    }
-
-    //hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f
-    const digitsRegex = /^#([0-9]|[a-f]){6}$/;
-    // const lettersRegex = /^#[a-f]{6}$/;
-
-    if (!digitsRegex.test(current.hcl)) {
-      continue;
-    }
-
-    // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    const validColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
-
-    if (!validColors.includes(current.ecl)) {
-      continue;
-    }
-
-    //pid (Passport ID) - a nine-digit number, including leading zeroes.
-
-    const pidRegex = /^[0-9]{9}$/;
-    if (!pidRegex.test(current.pid)) {
-      continue;
-    }
-
-    numValid += 1;
+  if (!['cm', 'in'].includes(unit)) {
+    return false;
   }
 
-  return numValid;
+  return unit === 'cm'
+    ? number >= 150 && number <= 193
+    : number >= 59 && number <= 76;
 };
 
-console.log('part 1', part1());
-console.log('part 2', part2());
+const isValidHcl = (hcl) => {
+  const digitsRegex = /^#([0-9]|[a-f]){6}$/;
+
+  return digitsRegex.test(hcl);
+};
+
+const isValidEcl = (ecl) => EYE_COLOR.includes(ecl);
+
+const isValidPid = (pid) => {
+  const pidRegex = /^[0-9]{9}$/;
+
+  return pidRegex.test(pid);
+};
+
+const part1 = (passports) => {
+  return passports.filter((passport) =>
+    REQUIRED_PASSPORT_FIELDS.every((requiredField) =>
+      Object.keys(passport).includes(requiredField)
+    )
+  ).length;
+};
+
+const part2 = (passports) => {
+  return passports.filter((passport) => {
+    return (
+      REQUIRED_PASSPORT_FIELDS.every((requiredField) =>
+        Object.keys(passport).includes(requiredField)
+      ) &&
+      valueIsBetween(passport.byr, 1920, 2002) &&
+      valueIsBetween(passport.iyr, 2010, 2020) &&
+      valueIsBetween(passport.eyr, 2010, 2030) &&
+      isValidHgt(passport.hgt) &&
+      isValidHcl(passport.hcl) &&
+      isValidEcl(passport.ecl) &&
+      isValidPid(passport.pid)
+    );
+  }).length;
+};
+
+const lines = fs
+  .readFileSync('./day-04/4.in', 'utf8')
+  .split('\n\n')
+  .map((a) => a.replace(/\n/g, ' '))
+  .map((p) => p.split(' '))
+  .map((a) =>
+    a.reduce((acc, cur) => {
+      const [key, val] = cur.split(':');
+
+      return {
+        ...acc,
+        [key]: val,
+      };
+    }, {})
+  );
+
+console.log('part 1', part1(lines));
+console.log('part 2', part2(lines));
