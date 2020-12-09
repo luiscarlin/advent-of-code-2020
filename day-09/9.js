04de01a679201f29c8fab8d8d66ff6316fa93d92
Luis Carlin <luiscarlin@gmail.com>
2020-12-09 00:37:02 -0500

    part 1 and part 2 of day 9 complete

---
+++ day-09/9.js
@@ -0,0 +1,70 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const part1 = () => {
+  const list = fs.readFileSync('./day-09/9.in', 'utf8').split('\n').map(Number);
+
+  let index = 25;
+  let startRange = 0;
+
+  while (true) {
+    const subList = list.slice(startRange, index);
+    const answer = list[index];
+
+    let isValid = false;
+
+    for (let i = 0; i < subList.length; i += 1) {
+      for (let x = 0; x < subList.length; x += 1) {
+        if (i === x) {
+          continue;
+        }
+
+        if (subList[i] + subList[x] === answer) {
+          isValid = true;
+          break;
+        }
+      }
+      if (isValid) {
+        break;
+      }
+    }
+
+    if (!isValid) {
+      return answer;
+    }
+
+    startRange += 1;
+    index += 1;
+  }
+};
+
+const part2 = () => {
+  const match = 507622668;
+
+  const list = fs.readFileSync('./day-09/9.in', 'utf8').split('\n').map(Number);
+
+  let index = 0;
+  let end = index + 2;
+  let nums;
+
+  while (true) {
+    const subList = list.slice(index, end);
+
+    const sum = _.sum(subList);
+
+    if (sum === match) {
+      nums = subList;
+      break;
+    } else if (sum < match) {
+      end += 1;
+    } else if (sum > match) {
+      index += 1;
+      end = index + 2;
+    }
+  }
+
+  return Math.min(...nums) + Math.max(...nums);
+};
+
+console.log('part 1', part1());
+console.log('part 2', part2());
