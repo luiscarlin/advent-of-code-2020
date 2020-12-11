4cca6f1e1f775536a01e59b63598f6db6a9d0173
Luis Carlin <luiscarlin@gmail.com>
2020-12-11 13:03:24 -0500

    much better and efficient

--- day-11/11.js
+++ day-11/11.js
@@ -1,175 +1,5 @@
 import fs from 'fs';
-
-const deepCopy = (currentArray) => currentArray.map((arr) => arr.slice());
-
-const getNeighbors = (row, col, grid) => {
-  const raw = [];
-
-  raw.push(grid[row + 1] && grid[row + 1][col]);
-  raw.push(grid[row - 1] && grid[row - 1][col]);
-  raw.push(grid[row][col + 1]);
-  raw.push(grid[row][col - 1]);
-
-  raw.push(grid[row + 1] && grid[row + 1][col + 1]);
-  raw.push(grid[row - 1] && grid[row - 1][col + 1]);
-  raw.push(grid[row - 1] && grid[row - 1][col - 1]);
-  raw.push(grid[row + 1] && grid[row + 1][col - 1]);
-
-  const neighbors = raw.filter((n) => !!n);
-
-  return {
-    occ: neighbors.filter((x) => x === '#').length,
-    empty: neighbors.filter((x) => x === 'L').length,
-  };
-};
-
-const getFirstSeenNeighbors = (row, col, grid) => {
-  let numEmpty = 0;
-  let numOcc = 0;
-
-  // left
-  for (let i = col - 1; i >= 0; i -= 1) {
-    if (grid[row][i] === 'L') {
-      numEmpty += 1;
-      break;
-    }
-    if (grid[row][i] === '#') {
-      numOcc += 1;
-      break;
-    }
-  }
-
-  // right
-  for (let i = col + 1; i < grid[0].length; i += 1) {
-    if (grid[row][i] === 'L') {
-      numEmpty += 1;
-      break;
-    }
-
-    if (grid[row][i] === '#') {
-      numOcc += 1;
-      break;
-    }
-  }
-
-  // up
-  for (let i = row - 1; i >= 0; i -= 1) {
-    if (grid[i][col] === 'L') {
-      numEmpty += 1;
-      break;
-    }
-
-    if (grid[i][col] === '#') {
-      numOcc += 1;
-      break;
-    }
-  }
-
-  // down
-  for (let i = row + 1; i < grid.length; i += 1) {
-    if (grid[i][col] === 'L') {
-      numEmpty += 1;
-      break;
-    }
-
-    if (grid[i][col] === '#') {
-      numOcc += 1;
-      break;
-    }
-  }
-
-  // up left
-  let index = 1;
-
-  while (true) {
-    const value = grid[row - index] && grid[row - index][col - index];
-
-    if (!value) {
-      break;
-    }
-
-    if (value === 'L') {
-      numEmpty += 1;
-      break;
-    }
-    if (value === '#') {
-      numOcc += 1;
-      break;
-    }
-    index += 1;
-  }
-
-  // up right
-
-  index = 1;
-
-  while (true) {
-    const value = grid[row - index] && grid[row - index][col + index];
-
-    if (!value) {
-      break;
-    }
-
-    if (value === 'L') {
-      numEmpty += 1;
-      break;
-    }
-    if (value === '#') {
-      numOcc += 1;
-      break;
-    }
-    index += 1;
-  }
-
-  // down left
-
-  index = 1;
-
-  while (true) {
-    const value = grid[row + index] && grid[row + index][col - index];
-
-    if (!value) {
-      break;
-    }
-
-    if (value === 'L') {
-      numEmpty += 1;
-      break;
-    }
-    if (value === '#') {
-      numOcc += 1;
-      break;
-    }
-    index += 1;
-  }
-
-  // down right
-
-  index = 1;
-
-  while (true) {
-    const value = grid[row + index] && grid[row + index][col + index];
-
-    if (!value) {
-      break;
-    }
-
-    if (value === 'L') {
-      numEmpty += 1;
-      break;
-    }
-    if (value === '#') {
-      numOcc += 1;
-      break;
-    }
-    index += 1;
-  }
-
-  return {
-    occ: numOcc,
-    empty: numEmpty,
-  };
-};
+import _ from 'lodash';
 
 const part1 = () => {
   let grid = fs
@@ -177,19 +7,37 @@ const part1 = () => {
     .split('\n')
     .map((line) => line.split(''));
 
+  const ROWS = grid.length;
+  const COLS = grid[0].length;
+
   while (true) {
-    let nextGen = deepCopy(grid);
+    let nextGen = _.cloneDeep(grid);
 
     let hasAnythingChanged = false;
 
-    for (let row = 0; row < grid.length; row += 1) {
-      for (let col = 0; col < grid[0].length; col += 1) {
-        const { occ, empty } = getNeighbors(row, col, grid);
+    for (let row of _.range(ROWS)) {
+      for (let col of _.range(COLS)) {
+        let neighOcc = 0;
 
-        if (grid[row][col] === 'L' && occ === 0) {
+        // get number occupied neighbors
+        for (let dr of [-1, 0, 1]) {
+          for (let dc of [-1, 0, 1]) {
+            if (dr === 0 && dc === 0) {
+              continue;
+            }
+
+            const neighbor = grid[row + dr] && grid[row + dr][col + dc];
+
+            if (neighbor && neighbor === '#') {
+              neighOcc += 1;
+            }
+          }
+        }
+
+        if (grid[row][col] === 'L' && neighOcc === 0) {
           nextGen[row][col] = '#';
           hasAnythingChanged = true;
-        } else if (grid[row][col] === '#' && occ >= 4) {
+        } else if (grid[row][col] === '#' && neighOcc >= 4) {
           nextGen[row][col] = 'L';
           hasAnythingChanged = true;
         } else {
@@ -199,20 +47,20 @@ const part1 = () => {
     }
 
     if (!hasAnythingChanged) {
-      let numberOcc = 0;
+      let totalNumberOcc = 0;
 
       for (let row = 0; row < grid.length; row += 1) {
         for (let col = 0; col < grid[0].length; col += 1) {
           if (grid[row][col] === '#') {
-            numberOcc += 1;
+            totalNumberOcc += 1;
           }
         }
       }
 
-      return numberOcc;
+      return totalNumberOcc;
     }
 
-    grid = deepCopy(nextGen);
+    grid = _.cloneDeep(nextGen);
   }
 };
 
@@ -222,19 +70,47 @@ const part2 = () => {
     .split('\n')
     .map((line) => line.split(''));
 
+  const ROWS = grid.length;
+  const COLS = grid[0].length;
+
   while (true) {
-    let nextGen = deepCopy(grid);
+    let nextGen = _.cloneDeep(grid);
 
     let hasAnythingChanged = false;
 
-    for (let row = 0; row < grid.length; row += 1) {
-      for (let col = 0; col < grid[0].length; col += 1) {
-        const { occ, empty } = getFirstSeenNeighbors(row, col, grid);
+    for (let row of _.range(ROWS)) {
+      for (let col of _.range(COLS)) {
+        let neighOcc = 0;
 
-        if (grid[row][col] === 'L' && occ === 0) {
+        // get number occupied neighbors
+        for (let dr of [-1, 0, 1]) {
+          for (let dc of [-1, 0, 1]) {
+            if (dr === 0 && dc === 0) {
+              continue;
+            }
+
+            let r = row + dr;
+            let c = col + dc;
+
+            let neighbor = grid[r] && grid[r][c];
+
+            while (neighbor === '.') {
+              r += dr;
+              c += dc;
+
+              neighbor = grid[r] && grid[r][c];
+            }
+
+            if (neighbor && neighbor === '#') {
+              neighOcc += 1;
+            }
+          }
+        }
+
+        if (grid[row][col] === 'L' && neighOcc === 0) {
           nextGen[row][col] = '#';
           hasAnythingChanged = true;
-        } else if (grid[row][col] === '#' && occ >= 5) {
+        } else if (grid[row][col] === '#' && neighOcc >= 5) {
           nextGen[row][col] = 'L';
           hasAnythingChanged = true;
         } else {
@@ -244,21 +120,22 @@ const part2 = () => {
     }
 
     if (!hasAnythingChanged) {
-      let numberOcc = 0;
+      let totalNumberOcc = 0;
 
       for (let row = 0; row < grid.length; row += 1) {
         for (let col = 0; col < grid[0].length; col += 1) {
           if (grid[row][col] === '#') {
-            numberOcc += 1;
+            totalNumberOcc += 1;
           }
         }
       }
 
-      return numberOcc;
+      return totalNumberOcc;
     }
 
-    grid = deepCopy(nextGen);
+    grid = _.cloneDeep(nextGen);
   }
 };
+
 console.log('part 1', part1());
 console.log('part 2', part2());
