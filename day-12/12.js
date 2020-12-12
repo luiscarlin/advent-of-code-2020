780e4e78f019540d3e5d2508e7b88415ddd46526
Luis Carlin <luiscarlin@gmail.com>
2020-12-12 01:23:58 -0500

    finally done

---
+++ day-12/12.js
@@ -0,0 +1,140 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const part1 = () => {
+  const lines = fs.readFileSync('./day-12/12.in', 'utf8').split('\n');
+
+  let x = 0;
+  let y = 0;
+  let dir = 'E';
+
+  const coord = ['N', 'E', 'S', 'W'];
+
+  for (let i of lines) {
+    const direction = i[0];
+    const amount = +i.slice(1);
+
+    if (direction === 'N') {
+      y += amount;
+    }
+
+    if (direction === 'S') {
+      y -= amount;
+    }
+
+    if (direction === 'W') {
+      x -= amount;
+    }
+
+    if (direction === 'E') {
+      x += amount;
+    }
+
+    if (direction === 'F') {
+      if (dir === 'N') {
+        y += amount;
+      }
+      if (dir === 'S') {
+        y -= amount;
+      }
+      if (dir === 'W') {
+        x -= amount;
+      }
+      if (dir === 'E') {
+        x += amount;
+      }
+    }
+
+    if (direction === 'L') {
+      const turns = (amount / 90) % 4;
+
+      let index = coord.indexOf(dir);
+
+      for (let i of _.range(turns)) {
+        index -= 1;
+
+        if (index === -1) {
+          index = 3;
+        }
+      }
+
+      dir = coord[index];
+    }
+
+    if (direction === 'R') {
+      const turns = (amount / 90) % 4;
+
+      let index = coord.indexOf(dir);
+
+      for (let i of _.range(turns)) {
+        index += 1;
+
+        if (index === 4) {
+          index = 0;
+        }
+      }
+
+      dir = coord[index];
+    }
+  }
+  return Math.abs(x) + Math.abs(y);
+};
+
+const part2 = () => {
+  const lines = fs.readFileSync('./day-12/12.in', 'utf8').split('\n');
+
+  let wx = 10;
+  let wy = 1;
+  let x = 0;
+  let y = 0;
+
+  for (let i of lines) {
+    const direction = i[0];
+    const amount = +i.slice(1);
+
+    if (direction === 'N') {
+      wy += amount;
+    }
+
+    if (direction === 'S') {
+      wy -= amount;
+    }
+
+    if (direction === 'E') {
+      wx += amount;
+    }
+
+    if (direction === 'W') {
+      wx -= amount;
+    }
+
+    if (direction === 'F') {
+      x += amount * wx;
+      y += amount * wy;
+    }
+
+    if (direction === 'L') {
+      const turns = amount / 90;
+
+      for (let i of _.range(turns)) {
+        let orgWx = wx;
+        wx = -wy;
+        wy = orgWx;
+      }
+    }
+
+    if (direction === 'R') {
+      const turns = amount / 90;
+      for (let i of _.range(turns)) {
+        let orgWx = wx;
+        wx = wy;
+        wy = -orgWx;
+      }
+    }
+  }
+
+  return Math.abs(x) + Math.abs(y);
+};
+
+console.log('part 1', part1());
+console.log('part 2', part2());
